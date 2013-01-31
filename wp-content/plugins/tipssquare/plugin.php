cd61066f01a176bcb9a37533a1ccc6ba25d460b8
Patrick Rauland <bftrick@gmail.com>
2013-01-30 21:34:50 -0600

    adjusting formatting of code

--- wp-content/plugins/tipssquare/plugin.php
+++ wp-content/plugins/tipssquare/plugin.php
@@ -340,42 +340,57 @@ add_filter( 'map_meta_cap', 'my_map_meta_cap', 10, 4 );
 
 function my_map_meta_cap( $caps, $cap, $user_id, $args ) {
 
-	/* If editing, deleting, or reading a movie, get the post and post type object. */
-	if ( 'edit_venue' == $cap || 'delete_venue' == $cap || 'read_venue' == $cap ) {
+	// If editing, deleting, or reading a movie, get the post and post type object. 
+	if ( 'edit_venue' == $cap || 'delete_venue' == $cap || 'read_venue' == $cap ) 
+	{
 		$post = get_post( $args[0] );
 		$post_type = get_post_type_object( $post->post_type );
 
-		/* Set an empty array for the caps. */
+		// Set an empty array for the caps. */
 		$caps = array();
 	}
 
-	/* If editing a venue, assign the required capability. */
-	if ( 'edit_venue' == $cap ) {
+	// If editing a venue, assign the required capability. 
+	if ( 'edit_venue' == $cap ) 
+	{
 		if ( $user_id == $post->post_author )
+		{
 			$caps[] = $post_type->cap->edit_posts;
+		}
 		else
+		{
 			$caps[] = $post_type->cap->edit_others_posts;
+		}
 	}
-
-	/* If deleting a venue, assign the required capability. */
-	elseif ( 'delete_venue' == $cap ) {
+	// If deleting a venue, assign the required capability. 
+	elseif ( 'delete_venue' == $cap ) 
+	{
 		if ( $user_id == $post->post_author )
+		{
 			$caps[] = $post_type->cap->delete_posts;
+		}
 		else
+		{
 			$caps[] = $post_type->cap->delete_others_posts;
+		}
 	}
-
-	/* If reading a private venue, assign the required capability. */
-	elseif ( 'read_venue' == $cap ) {
-
+	// If reading a private venue, assign the required capability. */
+	elseif ( 'read_venue' == $cap ) 
+	{
 		if ( 'private' != $post->post_status )
+		{
 			$caps[] = 'read';
+		}
 		elseif ( $user_id == $post->post_author )
+		{
 			$caps[] = 'read';
+		}
 		else
+		{
 			$caps[] = $post_type->cap->read_private_posts;
+		}
 	}
 
-	/* Return the capabilities required by the user. */
+	// Return the capabilities required by the user. */
 	return $caps;
 }
