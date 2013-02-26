019e15c2b89854206f435d3464d4c16023753750
Patrick Rauland <bftrick@gmail.com>
2013-02-25 20:24:21 -0600

    adding content to footer

--- wp-content/themes/tipssquare/footer.php
+++ wp-content/themes/tipssquare/footer.php
@@ -14,9 +14,8 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<?php do_action( 'tipssquare_credits' ); ?>
-			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'tipssquare' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'tipssquare' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'tipssquare' ), 'tipssquare', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			<p>TipsSquare was built with all the love in the world by <a href="http://www.patrickrauland.com/" target="_blank">Patrick Rauland</a> at <a href="http://www.kavarna.com/" target="_blank">Kavarna</a> &amp; <a href="http://www.lunacafe.com/" target="_blank">Luna Cafe</a> in sleepy Green Bay, WI.</p>
+			<p>TipsSquare is built on the incredible <a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'tipssquare' ); ?>" rel="generator">WordPress</a> platform</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon .site-footer -->
 </div><!-- #page .hfeed .site -->
